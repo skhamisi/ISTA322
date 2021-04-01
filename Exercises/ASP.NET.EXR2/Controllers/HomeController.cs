097931c7dd1935f6e653bc51f98fbe33c7de1456
Samer Khamisi <samer.n.khamisi@outlook.com>
2021-04-01 08:17:08 -0400

    added new ASP.NET EXR

---
+++ Exercises/ASP.NET.EXR2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using ASP.NET.EXR2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.EXR2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var samer = new[]
+            {
+                new { Name = "Samer", Age = 30, Wife = "Christine", FavoriteIceCream = "Neapolitan", Dog = "Allie", DogBreed = "Lab" }
+            };
+            return View(samer.Select(p => $"Name: {p.Name}, Age: {p.Age}, Wife: {p.Wife}, FavoriteIceCream: {p.FavoriteIceCream}, " +
+            $"Dog: {p.Dog}, DogBreed: {p.DogBreed}"));
+        }
+    }
+}
---
+++ Exercises/ASP.NET.EXR2/Models/Information.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace ASP.NET.EXR2.Models
+{
+    public class Information
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Wife { get; set; }
+        public string FavoriteIceCream { get; set; }
+        public string Dog { get; set; }
+        public string DogBreed { get; set; }
+
+        //public static Information[] GetInformation()
+        //{
+        //    Information samer = new()
+        //    {
+        //        Name = "Samer",
+        //        Age = 30,
+        //        Wife = "Christine",
+        //        FavoriteIceCream = "Neapolitan",
+        //        Dog = "Allie",
+        //        DogBreed = "Lab"
+        //    };
+        //    return new Information[] { samer, null };
+        //}
+    }
+}
